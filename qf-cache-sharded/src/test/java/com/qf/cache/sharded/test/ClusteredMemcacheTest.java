3be5d3f4b7411f6885015fe4a417ab0ec3e266df
qiongqi <qiongqi@ixiaopu.com>
2017-05-07 16:55:45 +0800

    memcache测试用例

---
+++ qf-cache-sharded/src/test/java/com/qf/cache/sharded/test/ClusteredMemcacheTest.java
@@ -0,0 +1,95 @@
+package com.qf.cache.sharded.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.qf.cache.exception.CacheOperateException;
+import com.qf.cache.sharded.memcache.ClusteredMemcache;
+
+import net.rubyeye.xmemcached.MemcachedClient;
+import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.XMemcachedClientBuilder;
+import net.rubyeye.xmemcached.utils.AddrUtil;
+
+public class ClusteredMemcacheTest {
+	
+	private Logger log = LoggerFactory.getLogger(ClusteredMemcacheTest.class);
+	
+	ClusteredMemcache cache;
+	String namespace = "com.qf.cache.memcache";
+	
+	@Before
+	public void before() throws Exception {
+		MemcachedClientBuilder builder = new XMemcachedClientBuilder((AddrUtil.getAddresses("192.168.48.128:11211")));
+		MemcachedClient client = builder.build();
+		cache = ClusteredMemcache.instance(namespace);
+		cache.setMemcachedClient(client);
+	}
+	
+	@Test
+	public void test() throws CacheOperateException, InterruptedException {
+		Map<String, Person> map = new HashMap<String, Person>();
+		map.put("zx", new Person("Zhang Xiang", 171, 145));
+		map.put("lkf", new Person("Li Ke Fu", 171, 168));
+		map.put("jzq", new Person("Jiang Zhi Qiang", 168, 140));
+		cache.put(map, 2000L, null);
+		Thread.sleep(1900);
+		List<Person> list = cache.get(new String[] { "zx", "lkf", "jzq" }, Person.class);	
+		for (Person p : list) {
+			log.error(p.getName() + ": " + p);
+		}
+		cache.evict(new String[] { "zx", "lkf", "jzq" });
+	}
+	
+	class Person {
+		
+		public Person() {}
+		
+		public Person(String name, float height, float weight) {
+			this.name = name;
+			this.height = height;
+			this.weight = weight;
+		}
+		
+		String name;
+		float height;
+		float weight;
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public float getHeight() {
+			return height;
+		}
+		
+		public void setHeight(float height) {
+			this.height = height;
+		}
+		
+		public float getWeight() {
+			return weight;
+		}
+		
+		public void setWeight(float weight) {
+			this.weight = weight;
+		}
+		
+		@Override
+		public String toString() {
+			return "name: " + name + " height: " + height + " weight: " + weight;
+		}
+		
+	}	
+	
+}
